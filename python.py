bf63bb001289ba79d362220151f89a16edb7c4ee
Awais Mumtaz <awaismumtaz1406@gmail.com>
2026-01-16 22:51:38 +0500

    Add files via upload

---
+++ python.py
@@ -0,0 +1,58 @@
+
+
+
+
+# print("Hello, World!")
+# s=0
+# l1 = [1,22,3,34,4,5,0]
+# c=d=0
+# for i in l1:
+#     s=s+i    
+# print("sum:",s)        
+# print("0s are:",c +"others are",d)
+
+# a=(2,3,4,5,3)
+
+# print(a)
+# b=a.count(2)
+# print(b)
+#      s=s+l1[a]
+# print(s)    
+# print(l1)
+# l1.ins
+# l1.insert(1,9)
+#  l1.pop(3)
+# l1.reverse()
+# print(l1)
+
+# l=int(input("enter name:"))
+
+a=()
+a=(1,)
+a=(1,2,7)
+print(a)
+# s="awais"
+# str=s.find("a")
+# print(str)
+# str=s.replace("a","A")
+# print(str)
+# print(f"length is {len(s)}")
+# print(s.endswith("s"))
+
+# print(s.count("a"))
+# letter = "Dear Ali,\nYou are selected!\nDate: 01-01-2026"
+# print(letter)
+
+
+# str="malikawaisghallu"
+# str.count('a')
+# str.find('u')
+# for i in str:
+#     print(i)
+
+
+# print(s[::-1])
+# print(list[::-1])
+# for i in s:
+#     print(i)
+
