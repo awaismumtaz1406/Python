94ef0f7b7ed10fe9907122f3049fc510c18bf95e
awaismumtaz1406 <awaismumtaz1406@gmail.com>
2026-03-17 03:47:10 +0500

    Convert Python folder from submodule to normal folder

---
+++ Python/lists.py
@@ -0,0 +1,26 @@
+
+print("before ")
+l1=[7,9,3,4,5,8]
+
+print(l1[2])
+for i in range(0,6):
+    print(l1[i])
+
+print("after ")
+
+print(l1.pop(3))
+l1.reverse()
+
+l1.remove()
+
+for i in range(0,6):
+ print(l1[i])
+
+
+# ///tuple
+
+
+a=(3,4,5,6,77)
+print()
+
+
