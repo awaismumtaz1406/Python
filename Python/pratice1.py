f20733e4fcd90cf0fe92717d4bcb540013d50312
awaismumtaz1406 <awaismumtaz1406@gmail.com>
2026-03-17 05:58:52 +0500

    new file created

---
+++ Python/pratice1.py
@@ -0,0 +1,24 @@
+s="awais"
+str=s.find("a")
+print(str)
+str=s.replace("a","A")
+print(str)
+print(f"length is {len(s)}")
+print(s.endswith("s"))
+
+print(s.count("a"))
+letter = "Dear Ali,\nYou are selected!\nDate: 01-01-2026"
+print(letter)
+
+
+str="malikawaisghallu"
+str.count('a')
+str.find('u')
+for i in str:
+    print(i)
+
+
+print(s[::-1])
+print(list[::-1])
+for i in s:
+    print(i)
