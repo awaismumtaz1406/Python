2450800148ebd431b8a82df21c4c6750008d12be
awaismumtaz1406 <awaismumtaz1406@gmail.com>
2026-03-19 05:39:04 +0500

    created a new file

--- Python/filehandling.py
+++ Python/filehandling.py
@@ -1,7 +1,24 @@
 
 
-try:
-    f = open("C:/Users/LAPTOP LEGENDS/Pictures/python/Python/test.txt", "r")
-    print(f.read())
-except FileNotFoundError:
-    print("File not found. Check location.")
+# try:
+#     f = open("C:/Users/LAPTOP LEGENDS/Pictures/python/Python/test.txt", "r")
+#     print(f.read())
+# except FileNotFoundError:
+#     print("File not found. Check location.")
+
+
+
+# f=open("C:/Users/LAPTOP LEGENDS/Pictures/python/Python/test.txt", "w")
+# f.write("Hello, World!")
+# f.close()
+
+# f=open("C:/Users/LAPTOP LEGENDS/Pictures/python/Python/test.txt", "r")
+# print(f.read())
+# f.close()
+
+
+
+##create a file and write to it
+f=open("C:/Users/LAPTOP LEGENDS/Pictures/python/Python/test.txt", "w")
+f.write("malik awais ghallu")
+f.close()
