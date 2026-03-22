11eb1b2af43edfa86f2906347f8a45f49f8c79ce
awaismumtaz1406 <awaismumtaz1406@gmail.com>
2026-03-23 04:23:30 +0500

    initial  commit

---
+++ Python/arithmeticoperationinnumpy.py
@@ -0,0 +1,9 @@
+import numpy as a
+list1=a.array_split(a.array([[10,20,30],[40,50,60],[70,80,90]]), 3)
+list2=a.array_split(a.array([[1,2,3],[4,5,6],[7,8,9]]), 3)
+list3=a.array+a.split([[11,22,33],[44,55,66],[77,88,99]])
+z=list1+list2+list3
+
+
+
+print(z)
