8694ad8adf51c2a80b11926a5d67148185c61fce
awaismumtaz1406 <awaismumtaz1406@gmail.com>
2026-03-24 16:17:42 +0500

    praticeed questions to improve hands on experience

--- Python/quiz.py
+++ Python/quiz.py
@@ -28,23 +28,36 @@
 
 # print(unique)             
 
-def getmarks(x):
-  return x[1]
+# def getmarks(x):
+#   return x[1]
 
-tuple=((1,10), (2,20), (3,30),(4,40),(5,50),(8,60), (9,70))
-# for i in range(0,len(tuple)-1):
-res=max(tuple,key=lambda x:x[1])
-print(res)
-
+# tuple=((1,10), (2,20), (3,30),(4,40),(5,50),(8,60), (9,70))
+# res=max(tuple,key=lambda x:x[1])
+# print(res)
 
 
+# data = (1, 'a', 3.5, 4, '5', 6)
+# res=[]
+# for x in data:
+#    if isinstance(x,str):
+#       res.append(x)
+# res=tuple(res)
+# print(res)
 
 
 
+# t = (1, 2, 2, 3, 1, 4)
+# uniq=[]
+# for i in t:
+#    if i not in uniq:
+#       uniq.append(i)
+# t=tuple(uniq)
+# print(t)      
+      
 
+# app1 = {"Ali", "Ahmed", "Sara"}
+# app2 = {"Sara", "Zain", "Ali"}
 
-
-
-
-
+# res=app1 or app2
+# print(res)
 
---
+++ Python/quiz2.py
@@ -0,0 +1,59 @@
+
+emails = {"tet@gmail.com", "user@yahoo.com"}
+new = "Test@gmail.com"
+
+for e in emails:
+     if new.lower() in  e.lower():
+          print("yes exists ")
+     else:
+         print("not  exists ")       
+
+
+
+blocked=[x for x in attempts if attempts[i]>3]
+blocked=[]
+for i in attempts:
+     if attempts[i]>3:
+      blocked.append(u) 
+
+
+list1=([[10,110,30],[40,50,60],[70,80,90]])
+list2=[]
+for i in list1:
+    list1.extend(i)
+
+print(list1)    
+
+
+tuple = (1, 2, 3, 4)
+tuple=tuple([-1],)+tuple[1:-1]+tuple([1],)
+print(tuple)
+
+
+
+import math
+a=(4,5)
+x,y=a
+  #print(math.sqrt(a[0]**2+a[1]**2)) 
+print(math.sqrt(x**2+y**2))
+
+
+
+
+points = ((3,4), (5,12), (8,15))
+for x,y in points:
+    print(math.sqrt(x**2+y**2))
+
+
+
+A = {"Math", "Physics", "CS"}
+B = {"CS", "Biology"}
+
+common = A & B
+unique = A ^ B
+total = A | B
+
+print("Common:", common)
+print("Unique:", unique)
+print("Total:", total)
+
