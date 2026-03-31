dc8bb8a7cd452a59c61181db2dab35661b5dbfe1
awaismumtaz1406 <awaismumtaz1406@gmail.com>
2026-03-31 16:24:24 +0500

    practiced new problems

--- Python/quiz3.py
+++ Python/quiz3.py
@@ -31,10 +31,10 @@ print(nn)
 # month=calendar.month(2026,2)
 # print(month)
 
-import calendar
-from turtle import pd
-month=calendar.isleap(2026)
-print(month)
+# import calendar
+# from turtle import pd
+# month=calendar.isleap(2026)
+# print(month)
 
 
 # from datetime import datetime
@@ -68,12 +68,74 @@ print(month)
 
 
 
-def creater():
-  i=1
-  while i<=10:
-    list.append(i)
-    i+=1
-print(creater())
-x=creater()
-print(next(x))
+# def creater():
+#   i=1
+#   while i<=10:
+#     list.append(i)
+#     i+=1
+# print(creater())
+# x=creater()
+# print(next(x))
+
+
+
+
+# list=[2,33,22,11,44,55]
+# l=list.sort(reverse=True)
+# print(l)
+
+
+
+
+# a=10
+# b=5
+# a=a+b
+# b=a-b
+# a=a-b
+# print(a)
+# print(b)
+
+# from collections import Counter, counter 
+# list1 = ['John','Kelly', 'Peter', 'Moses', 'Peter']
+# count_peter = Counter(list1).get("Peter")
+# print()
+
+
+list1 = [[1, 2, 3],[4, 5, 6]]
+# l=[]
+# for i in list1:
+#     for j in i:
+#        l.append(j)
+# print(l)       
+
+
+
+
+l=[i for j in list1 for i in j]
+print(l)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
