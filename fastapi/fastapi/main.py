feda1bcfd1c4d7db7cac63226ba0a59f84460f61
awaismumtaz1406 <awaismumtaz1406@gmail.com>
2026-04-26 22:45:55 +0500

    files added

--- fastapi/fastapi/main.py
+++ fastapi/fastapi/main.py
@@ -2,22 +2,22 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 from typing import List
 
-app = FastAPI()
+app= FastAPI()
 
 class Tea(BaseModel):
-    id: int
+    id: int 
     name: str
     origin: str
-
-teas: List[Tea] = []
-
-@app.get("/")
-def read_root():
-    return {"message": "Welcome to the Tea API!"}
-
-@app.get("/teas")
-def read_teas():
-    return teas
+    
+    teas: List[Tea] = []
+    
+    @appp.get("/")
+    def read_root():
+        return {"message": "Welcome to the Tea API!"}   
+    
+    @app.get("/teas")
+    def read_teas():
+        return teas()
   
     
     
