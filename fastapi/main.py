1c0bd0a877081065cbe21778a4a5d9b000eabc9c
awaismumtaz1406 <awaismumtaz1406@gmail.com>
2026-04-30 22:05:36 +0500

    files added

--- fastapi/main.py
+++ fastapi/main.py
@@ -39,6 +39,17 @@ def update_tea(tea_id: int, updated_tea: Tea):
     return {"error": "Tea not found"}
             
 
+@app.delete("/teas/{tea_id}")
+def delete_tea(tea_id:int):
+    for index, tea in enumerate(teas):
+        if tea.id==tea_id:
+            deleted=teas.pop(index)
+            return deleted
+    return {"error": "Tea not found"}
+
+
+
+
 # @app.put("/teas/{tea_id}")
 # def update_tea(tea_id:int , updated_tea: Tea):
 #     for i, t in enumerate(teas):
